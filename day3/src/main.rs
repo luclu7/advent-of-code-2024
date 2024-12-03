02ddea209472dbe8650d00c068caab8db2714336
Lucie Delestre <me@luclu7.fr>
2024-12-03 22:24:51 +0100

    Day 3 part 2 (début, j'ai pas encore les skills en regex qu'il faut)
    Signed-off-by: Lucie Delestre <me@luclu7.fr>

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,21 +3,25 @@ use std::fs;
 use std::ops::Mul;
 
 fn main() {
-    let re = Regex::new(r"mul\(([1-9]\d{0,2}),([1-9]\d{0,2})\)").unwrap();
+    let part1regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+
+    // let part2regex = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
 
     let text = fs::read_to_string("input.txt")
         .expect("erreur lors de la lecture du fichier")
         .replace("\n", ""); // on supprime les retours à la ligne, inutiles
 
-    let total =
-        re.captures_iter(&*text)
+    let total = |regex: Regex, text: &str| -> usize {
+        regex
+            .captures_iter(text)
             .map(|c| c.extract())
             .fold(0, |acc, (_, [x_str, y_str])| {
-                let x: isize = x_str.parse().expect("uh");
-                let y: isize = y_str.parse().expect("uh");
-                // println!("mul({},{})={}", x, y, x * y);
+                let x: usize = x_str.parse().expect("uh");
+                let y: usize = y_str.parse().expect("uh");
                 return acc + (x.mul(y));
-            });
+            })
+    };
 
-    println!("Total (part 1): {}", total);
+    println!("Total (part 1): {}", total(part1regex, &text));
+    // println!("Total (part 2): {}", total(part2regex, &text));
 }
